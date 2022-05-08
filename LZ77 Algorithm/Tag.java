e03502ef5e4dd8785543ee58f31d1be6357a5370
Mahmoud-AbdElghany-Ebrahim <83129031+Mahmoud-AbdElghany-Ebrahim@users.noreply.github.com>
2022-05-09 01:16:18 +0200

    Update Tag.java

--- LZ77 Algorithm/Tag.java
+++ LZ77 Algorithm/Tag.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.lz77;
 
 /**
  *
- * @author user
+ * @author Mahmoud Abd El-Ghany
  */
 public class Tag {
     private int position;
